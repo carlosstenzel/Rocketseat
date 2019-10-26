d5c602a243ca67ce564fb8c465f110b4a32fb6c8
carlosstenzel <carlosstenzel@hotmail.com>
2019-10-25 21:46:05 -0300

    Middleware de autenticação

---
+++ modulo02/src/app/middleware/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeaader = req.headers.authorization;
+
+  if (!authHeaader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeaader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
