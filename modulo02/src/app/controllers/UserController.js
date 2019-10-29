626d6061ae61e4a7619e3dc1920868e1416efc7f
carlosstenzel <carlosstenzel@hotmail.com>
2019-10-28 21:48:29 -0300

    Update do usuario

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -19,7 +19,35 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+    //verifica se email igual o cadastado
+    if(email != user.email) {
+      // verifica se usuario existe
+      // se email existe
+      const userExists = await User.findOne({ where: { email } });
+      if(userExists) {
+        return res.status(400).json({ error: "User already exist." });
+      }
+    }
+
+    // verifica se senha antiga esta correta
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const user = await user.update(req.body);
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  
   }
 
 }
