92130e7262a2fa318587b13294b14a32fdf37743
carlosstenzel <carlosstenzel@hotmail.com>
2019-11-06 23:08:19 -0300

    Configurando MongoDB

--- modulo02.01/src/database/index.js
+++ modulo02.01/src/database/index.js
@@ -1,15 +1,16 @@
-import Sequelize from 'sequelize';
-import Users from '../app/models/User';
-import File from '../app/models/File';
-import Appointment from '../app/models/Appointment';
-import databaseConfig from '../config/database';
+import Sequelize from "sequelize";
+import mongoose from "mongoose";
+import Users from "../app/models/User";
+import File from "../app/models/File";
+import Appointment from "../app/models/Appointment";
+import databaseConfig from "../config/database";
 
 const models = [Users, File, Appointment];
 
 class Database {
-
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -17,6 +18,16 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      "mongodb://localhost:32769/gobarber",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    );
   }
 }
 
