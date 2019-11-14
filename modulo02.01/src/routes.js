41842503562da4a095657b97f68532f70cdda372
carlosstenzel <carlosstenzel@hotmail.com>
2019-11-14 15:59:51 -0300

    Listando horarios disponiveis

--- modulo02.01/src/routes.js
+++ modulo02.01/src/routes.js
@@ -11,6 +11,7 @@ import authMiddleware from './app/middleware/auth';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -25,6 +26,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
