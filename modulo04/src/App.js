00b975f7d4f7a84ad730b6bb16eee771bd0e78ce
carlosstenzel <carlosstenzel@hotmail.com>
2019-11-29 07:37:18 -0300

    class e components

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import "./App.css";
 
-import profile from './assets/fotoperfil.jpg';
+import TechList from './components/TechList';
 
 function App() {
-  return <img width="60" src={profile} />
+  return <TechList />
 }
 
 export default App;
