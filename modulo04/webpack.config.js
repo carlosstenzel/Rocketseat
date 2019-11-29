6aba93696ce65414a36364152a91e4522c96e596
carlosstenzel <carlosstenzel@hotmail.com>
2019-11-28 22:20:20 -0300

    importando css

--- modulo04/webpack.config.js
+++ modulo04/webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
           loader: 'babel-loader'
         }
 
+      }, {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader'},
+          { loader: 'css-loader'},
+        ]
       }
     ]
   }
