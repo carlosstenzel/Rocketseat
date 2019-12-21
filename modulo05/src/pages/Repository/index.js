67cdc39873a3b229d5c72c5e3a85f3aa700b2169
carlosstenzel <carlosstenzel@hotmail.com>
2019-12-21 19:30:11 -0300

    carregando dados via api

--- modulo05/src/pages/Repository/index.js
+++ modulo05/src/pages/Repository/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repository: {},
+      issues: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        state: 'Open',
+        per_page: 5,
+      }),
+    ]);
+
+    this.setState({
+      repostitory: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+
+
+    return <h1>Repository: </h1>;
+  }
 }
