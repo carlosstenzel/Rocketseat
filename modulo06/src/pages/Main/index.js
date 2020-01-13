122824ee42ab3d6d221183d7a2a526b596a44fe4
carlosstenzel <carlosstenzel@hotmail.com>
2020-01-13 20:44:10 -0300

    create config axios

--- modulo06/src/pages/Main/index.js
+++ modulo06/src/pages/Main/index.js
@@ -4,23 +4,38 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {Container, Form, Input, SubmitButton} from './styles';
 
-export default function Main() {
-  return (
-    <>
-      <Container>
-        <Form>
-          <Input
-            autoCorrect={false}
-            autoCapitalize="none"
-            placeholder="Adicionar usuários"
-          />
-          <SubmitButton>
-            <Icon name="add" size={20} color="#FFF" />
-          </SubmitButton>
-        </Form>
-      </Container>
-    </>
-  );
+export default class Main extends React.Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = () => {
+    console.log(this.state.newUser);
+  };
+
+  render() {
+    const {users, newUser} = this.state;
+    return (
+      <>
+        <Container>
+          <Form>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Adicionar usuários"
+              value={newUser}
+              onChangeText={text => this.setState({newUser: text})}
+              returnKeyType="send"
+            />
+            <SubmitButton onPress={this.handleAddUser}>
+              <Icon name="add" size={20} color="#FFF" />
+            </SubmitButton>
+          </Form>
+        </Container>
+      </>
+    );
+  }
 }
 
 Main.navigationOptions = {
