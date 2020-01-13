fd5b64199abbba30f92eeae3d03241f7007f5f70
carlosstenzel <carlosstenzel@hotmail.com>
2020-01-12 21:49:52 -0300

    estilizando formulario

--- modulo06/src/pages/Main/index.js
+++ modulo06/src/pages/Main/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import {Text} from 'react-native';
 
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, Form, Input, SubmitButton} from './styles';
 
 export default function Main() {
   return (
     <>
       <Container>
-        <Text>Ola</Text>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuários"
+          />
+          <SubmitButton>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
       </Container>
     </>
   );
