d51d23e4225b077276faca768b4bc7219fd72095
carlosstenzel <carlosstenzel@hotmail.com>
2020-01-13 19:51:37 -0300

    Iniciando OmmiStack 10.0

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -10,19 +10,23 @@ class TechList extends Component {
 
   // Executado assim que o componente aparece em tela
   componentDidMount() {
+    const techs = localStorage.getItem("techs");
 
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
   }
 
   // Executado sempre que houver alterações nas props ou estado
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     // this.props, this.state
-
+    if (prevState.techs != this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
   }
 
   // Executado quando o componente deixa de existir
-  componentWillUnmount() {
-
-  }
+  componentWillUnmount() {}
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
