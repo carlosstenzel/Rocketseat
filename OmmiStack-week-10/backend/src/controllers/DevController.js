e78a42fc21bfc051d182dbbc46f068b0d793378f
carlosstenzel <carlosstenzel@hotmail.com>
2020-01-14 23:21:11 -0300

    Finaliza base app

--- OmmiStack-week-10/backend/src/controllers/DevController.js
+++ OmmiStack-week-10/backend/src/controllers/DevController.js
@@ -1,31 +1,41 @@
-const axios = require('axios');
-const Dev = require('../models/Dev');
+const axios = require("axios");
+const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+    return response.json(devs);
+  },
+
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
-    const apiResponse = await axios.get(
-      `https://api.github.com/users/${github_username}`
-    );
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = apiResponse.data;
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
 
-    const techsArrays = techs.split(",").map(tech => tech.trim());
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
-    const location = {
-      type: "Point",
-      coordinates: [longitude, latitude]
-    };
+      const techsArrays = parseStringAsArray(techs);
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArrays,
-      location
-    });
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArrays,
+        location
+      });
+    }
 
     return response.json(dev);
   }
