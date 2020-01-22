6ea3a531fdfc8bb6776503538e2792e2230a9ff3
carlosstenzel <carlosstenzel@hotmail.com>
2020-01-21 21:11:26 -0300

    Version mobile

---
+++ OmmiStack-week-10/mobile/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Routes from './src/routes'
+import {StatusBar, YellowBox} from 'react-native'
+
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+])
+
+export default function App() {
+  return (
+    <>
+    <StatusBar barStyle= "light-content" backgroundColor="#74de7"/>
+    <Routes />   
+    </>
+  );
+}
+
+
