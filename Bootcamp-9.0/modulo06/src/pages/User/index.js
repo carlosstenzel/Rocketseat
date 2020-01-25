d8aa9f6576b64067c9c5a1c762d5587e1e617cad
carlosstenzel <carlosstenzel@hotmail.com>
2020-01-24 22:17:46 -0300

    Styled page user and list star

--- Bootcamp-9.0/modulo06/src/pages/User/index.js
+++ Bootcamp-9.0/modulo06/src/pages/User/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View} from 'react-native';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends React.Component {
   static navigationOptions = ({navigation}) => ({
@@ -14,7 +25,7 @@ export default class User extends React.Component {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
     }).isRequired,
-  }
+  };
 
   state = {
     stars: [],
@@ -30,6 +41,32 @@ export default class User extends React.Component {
   }
 
   render() {
-    return <View />;
+    const {navigation} = this.props;
+    const {stars} = this.state;
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{uri: user.avatar}} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({item}) => (
+            <Starred>
+              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
