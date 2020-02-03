305f17fd72c97e1bee01f4c4b984ff742b47ba4f
carlosstenzel <carlosstenzel@hotmail.com>
2020-02-03 18:24:17 -0300

    hooks use Memo

--- Bootcamp-9.0/utilizando-react-hooks/src/App.js
+++ Bootcamp-9.0/utilizando-react-hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [tech, setTech] = useState(['ReactJS', 'React Native']);
@@ -26,6 +26,8 @@ function App() {
     localStorage.setItem('tech', JSON.stringify(tech));
   }, [tech]);
 
+  const techSize = useMemo(() => tech.length, [tech]);
+
   return (
     <>
       <ul>
@@ -33,6 +35,8 @@ function App() {
           <li key={t}>{t}</li>
         ))}
       </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
       <input
         type="text"
         value={newTech}
