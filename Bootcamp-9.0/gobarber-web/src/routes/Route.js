186b254656b89b6b34cd93421fab3258deaef124
carlosstenzel <carlosstenzel@hotmail.com>
2020-02-05 21:04:09 -0300

    layout por paginas

--- Bootcamp-9.0/gobarber-web/src/routes/Route.js
+++ Bootcamp-9.0/gobarber-web/src/routes/Route.js
@@ -1,6 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
+
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
 
 export default function RouteWrapper({
   component: Component,
@@ -17,8 +21,19 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />;
   }
 
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Route {...rest} component={Component} />;
+  return (
+    <Route
+      {...rest}
+      component={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
