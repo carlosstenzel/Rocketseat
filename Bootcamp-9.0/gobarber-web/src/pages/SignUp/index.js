8c36db0b35617bc4c45f71f458fc6a89fea06ea7
carlosstenzel <carlosstenzel@hotmail.com>
2020-02-06 22:08:16 -0300

    utilizando unform

--- Bootcamp-9.0/gobarber-web/src/pages/SignUp/index.js
+++ Bootcamp-9.0/gobarber-web/src/pages/SignUp/index.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
+  function handleSumit(data){
+
+  }
+
   return (
     <>
       <img src={logo} alt="Gobarber" />
 
-      <form>
-        <input type="text" placeholder="Nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSumit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
