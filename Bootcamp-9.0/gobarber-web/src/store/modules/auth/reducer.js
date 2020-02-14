c79bab53fbf919d84e3b7a413b6a41e3fc6a964b
carlosstenzel <carlosstenzel@hotmail.com>
2020-02-14 07:36:09 -0300

    exibindo toasts

--- Bootcamp-9.0/gobarber-web/src/store/modules/auth/reducer.js
+++ Bootcamp-9.0/gobarber-web/src/store/modules/auth/reducer.js
@@ -20,7 +20,7 @@ export default function auth(state = INITIAL_STATE, action) {
         break;
       }
       case '@auth/SIGN_FAILURE': {
-        draft.loading = true;
+        draft.loading = false;
         break;
       }
       default:
