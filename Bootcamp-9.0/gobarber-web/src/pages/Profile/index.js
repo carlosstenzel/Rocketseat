7cd15cf7fc8f85c6092a835633dff3cf2033af44
carlosstenzel <carlosstenzel@hotmail.com>
2020-02-20 07:37:02 -0300

    Pagina de perfil

--- Bootcamp-9.0/gobarber-web/src/pages/Profile/index.js
+++ Bootcamp-9.0/gobarber-web/src/pages/Profile/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>P</h1>;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova Senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
