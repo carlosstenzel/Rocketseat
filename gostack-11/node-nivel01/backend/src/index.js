ce230082a5bef417acf412585ea47dbda72a7d36
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-04-25 10:03:19 -0300

    Configurando Nodemon

--- gostack-11/node-nivel01/backend/src/index.js
+++ gostack-11/node-nivel01/backend/src/index.js
@@ -2,8 +2,10 @@ const express = require(`express`);
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello Word'});
-})
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello Word" });
+});
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("🚀 Back-en started!");
+});
