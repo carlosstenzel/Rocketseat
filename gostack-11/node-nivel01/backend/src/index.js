68a8f6fc363a800666c9ecabe53095d04f1845ba
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-04-25 10:15:26 -0300

    Métodos HTTP

--- gostack-11/node-nivel01/backend/src/index.js
+++ gostack-11/node-nivel01/backend/src/index.js
@@ -2,9 +2,36 @@ const express = require(`express`);
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Word" });
+app.get("/projects", (request, response) => {
+  return response.json([
+    "Projeto 1",
+    "Projeto 2"
+  ]);
 });
+
+app.post("/projects", (request, response) => {
+  return response.json([
+    "Projeto 1",
+    "Projeto 2",
+    "Projeto 3"
+  ]);
+});
+
+app.put("/projects/:id", (request, response) => {
+  return response.json([
+    "Projeto 1",
+    "Projeto 2",
+    "Projeto 4"
+  ]);
+});
+
+app.delete("/projects:id", (request, response) => {
+  return response.json([
+    "Projeto 1",
+    "Projeto 2"
+  ]);
+});
+
 
 app.listen(3333, () => {
   console.log("🚀 Back-en started!");
