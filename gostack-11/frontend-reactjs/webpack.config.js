ee99eed5dfadaba877389fba5a1c502a20cbb7f9
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-04-27 22:35:55 -0300

    Important css loader

--- gostack-11/frontend-reactjs/webpack.config.js
+++ gostack-11/frontend-reactjs/webpack.config.js
@@ -18,6 +18,18 @@ module.exports = {
           loader: 'babel-loader'
         }
 
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+        ]
       }
     ]
   }
