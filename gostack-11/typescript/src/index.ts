ff356580bad7909fd2d8b39f953567fd4291520a
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-05-02 20:35:17 -0300

    TypeScript init project

---
+++ gostack-11/typescript/src/index.ts
@@ -0,0 +1,9 @@
+import express, { response, request } from "express";
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'hello'})
+})
+
+app.listen(3333);
