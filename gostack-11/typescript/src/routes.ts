f6165fb7ede32d555f9e08cb3c7b7128aa8ee540
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-05-02 21:01:36 -0300

    Tipando obejtos e vetores

--- gostack-11/typescript/src/routes.ts
+++ gostack-11/typescript/src/routes.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
 
 export function helloWord(request: Request, response: Response) {
+
+  const users = createUser({
+    email: 'carlosstenzel@hotmail.com',
+    password: '12345',
+    techs: [
+      'Node.js',
+      'React.Js',
+      { title: "Js", experience: 100},
+    ]
+  });
+  
   return response.json({ message: 'Hello Word'});
 }
