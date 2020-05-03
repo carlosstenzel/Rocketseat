fcab30dcf5be603cd0f88a40e31688b9e0fd51e8
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-05-03 13:27:07 -0300

    Validando a data

--- gostack-11/primeiro-projeto-node/src/routes/appointment.routes.ts
+++ gostack-11/primeiro-projeto-node/src/routes/appointment.routes.ts
@@ -1,17 +1,38 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  // Verifica se na data já existe algo cadastrado
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  // Se já estiver agendado horario
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({
+      message: 'This appointment is already booked',
+    });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
