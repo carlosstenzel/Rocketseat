0b69b2cb1aff9db32948fd6ffec52709d0b510a1
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-05-03 13:13:05 -0300

    cadastro de agendamento

--- gostack-11/primeiro-projeto-node/src/routes/index.ts
+++ gostack-11/primeiro-projeto-node/src/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointment.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello Word' }),
-);
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
