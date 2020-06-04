1ea7595e603dc1ff7ca1f7f6a8c0b9baea612ada
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-06-03 22:39:33 -0300

    Criando colunas created e updated

--- gostack-11/iniciando-back-end/src/database/migrations/1591057444487-CreateAppointments.ts
+++ gostack-11/iniciando-back-end/src/database/migrations/1591057444487-CreateAppointments.ts
@@ -24,6 +24,16 @@ export default class CreateAppointments1591057444487
             type: 'timestamp with time zone',
             isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
