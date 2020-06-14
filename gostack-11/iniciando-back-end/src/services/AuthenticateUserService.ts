183be86ead426d9d5521ce7ddf11d180a2a7b3cd
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-06-13 22:16:33 -0300

    Gerando token JWT

--- gostack-11/iniciando-back-end/src/services/AuthenticateUserService.ts
+++ gostack-11/iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -7,8 +8,13 @@ interface Request {
   password: string;
 }
 
+interface Response {
+  user: User;
+  token: string;
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<{ user: User }> {
+  public async execute({ email, password }: Request): Promise<Response> {
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne({ where: { email } });
@@ -23,8 +29,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'd0f29206576a432fe91a59c685785f0b', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
