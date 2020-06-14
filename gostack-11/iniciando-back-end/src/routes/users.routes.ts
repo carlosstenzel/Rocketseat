5fec4b9a84006804dd58c6213ce02c4b5686c8e8
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-06-14 08:36:24 -0300

    upload de arquivos

--- gostack-11/iniciando-back-end/src/routes/users.routes.ts
+++ gostack-11/iniciando-back-end/src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 
+import multer from 'multer';
 import CreateUserServices from '../services/CreateUserServices';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +27,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
