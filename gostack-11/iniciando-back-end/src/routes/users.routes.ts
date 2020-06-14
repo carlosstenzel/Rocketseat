b4ad28a687183b0ac1ffad04e4416478290f2167
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-06-14 09:04:18 -0300

    Atualizando Avatar

--- gostack-11/iniciando-back-end/src/routes/users.routes.ts
+++ gostack-11/iniciando-back-end/src/routes/users.routes.ts
@@ -4,6 +4,7 @@ import multer from 'multer';
 import CreateUserServices from '../services/CreateUserServices';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarServices';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -33,7 +34,19 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
