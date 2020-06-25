7cef26dc0af9a6fdf356c2e315d7e3d2b8d0d78f
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-06-24 21:11:25 -0300

    Isolando componentes

--- gostack-11/gobaber-web-type/src/pages/SignIn/index.tsx
+++ gostack-11/gobaber-web-type/src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
@@ -10,9 +13,14 @@ const SignIn: React.FC = () => {
         <img src={logoImg} alt="GoBarber" />
         <form>
           <h1>Faça seu logon</h1>
-          <input placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
-          <button type="submit">Entrar</button>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
         </form>
         <a href="cadastro">
