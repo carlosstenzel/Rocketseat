b48784fc49b550daf14cdf27085b5d7610bb65af
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-06-24 21:37:14 -0300

    utilizando unform

--- gostack-11/gobaber-web-type/src/pages/SignUp/index.tsx
+++ gostack-11/gobaber-web-type/src/pages/SignUp/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+
+import { Form } from '@unform/web';
+
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 
@@ -7,12 +10,16 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: []): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -23,7 +30,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="cadastro">
           <FiArrowLeft />
           Voltar para logon
