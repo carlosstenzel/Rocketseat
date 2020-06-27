50d57442a47c936f1544f8160d072681d7ff39b0
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-06-27 08:07:57 -0300

    API de Contexto

--- gostack-11/gobaber-web-type/src/App.tsx
+++ gostack-11/gobaber-web-type/src/App.tsx
@@ -3,10 +3,15 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import AutContext from './context/AuthContext';
+
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <AutContext.Provider value={{ name: 'Carlos' }}>
+        <SignIn />
+      </AutContext.Provider>
+
       <GlobalStyle />
     </>
   );
--- gostack-11/gobaber-web-type/src/pages/SignIn/index.tsx
+++ gostack-11/gobaber-web-type/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -9,9 +9,12 @@ import logoImg from '../../assets/logo.svg';
 import getValidationErros from '../../utils/getValidationErros';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import AuthContext from '../../context/AuthContext';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const auth = useContext(AuthContext);
 
   const handleSubmit = useCallback(async (data: []) => {
     try {
