b129bfdebe382f0cd8b545fb800b4d6bae438e1c
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-06-27 15:42:01 -0300

    Adicionando e removendo toasts

--- gostack-11/gobaber-web-type/src/components/ToastContainer/index.tsx
+++ gostack-11/gobaber-web-type/src/components/ToastContainer/index.tsx
@@ -1,40 +1,18 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import Toast from './Toast';
+import { ToastMessage } from '../../hooks/toast';
+import { Container } from './styles';
 
-import { Container, Toast } from './styles';
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
 
-const ToastContainer: React.FC = () => {
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
   return (
     <Container>
-      <Toast hasDescription>
-        <FiAlertCircle />
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <p>Não foi possivel fazer login na aplicação</p>
-        </div>
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-      <Toast type="success" hasDescription={false}>
-        <FiAlertCircle />
-        <div>
-          <strong>Aconteceu um erro</strong>
-        </div>
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-      <Toast type="error" hasDescription>
-        <FiAlertCircle />
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <p>Não foi possivel fazer login na aplicação</p>
-        </div>
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
+      {messages.map(message => (
+        <Toast key={message.id} message={message} />
+      ))}
     </Container>
   );
 };
