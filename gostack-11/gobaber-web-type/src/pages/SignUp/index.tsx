a0ff8683cc233c39c8d947bda3d2c4d0975d424e
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-06-26 21:09:16 -0300

    validando cadastro

--- gostack-11/gobaber-web-type/src/pages/SignUp/index.tsx
+++ gostack-11/gobaber-web-type/src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
-
+import * as Yup from 'yup';
 import { Form } from '@unform/web';
 
 import { Container, Content, Background } from './styles';
@@ -10,9 +10,21 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: []): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: []) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
