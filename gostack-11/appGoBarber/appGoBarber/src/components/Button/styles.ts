734b77ad2ee4b47522221400664abf2b2a10a3d5
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-07-02 22:59:50 -0300

    arruma button

--- gostack-11/appGoBarber/appGoBarber/src/components/Button/styles.ts
+++ gostack-11/appGoBarber/appGoBarber/src/components/Button/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  width: 100%;
   height: 60px;
   background: #ff9000;
   border-radius: 10px;
@@ -14,5 +13,6 @@ export const Container = styled(RectButton)`
 export const ButtonText = styled.Text`
   font-family: 'RobotoSlab-Medium';
   color: #312e38;
+
   font-size: 18px;
 `;
