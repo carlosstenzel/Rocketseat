d8d0edb2403c964aeec506f5bf16ce1297974f0e
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-07-04 14:53:05 -0300

    Validação dos formularios

--- gostack-11/appGoBarber/appGoBarber/src/components/Input/index.tsx
+++ gostack-11/appGoBarber/appGoBarber/src/components/Input/index.tsx
@@ -56,7 +56,7 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
       name: fieldName,
       ref: inputValueRef.current,
       path: 'value',
-      setValue(ref: any, value: string) {
+      setValue(value: string) {
         inputValueRef.current.value = value;
         inputElementRef.current.setNativeProps({ text: value });
       },
@@ -68,7 +68,7 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused}>
+    <Container isFocused={isFocused} isErrored={!!error}>
       <Icon
         name={icon}
         size={20}
--- gostack-11/appGoBarber/appGoBarber/src/components/Input/styles.ts
+++ gostack-11/appGoBarber/appGoBarber/src/components/Input/styles.ts
@@ -3,6 +3,7 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 
 interface ContainerProps {
   isFocused: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.View<ContainerProps>`
@@ -17,6 +18,11 @@ export const Container = styled.View<ContainerProps>`
   align-items: center;
   border-width: 2px;
   border-color: #232129;
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
 
   ${props =>
     props.isFocused &&
