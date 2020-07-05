17be81d7f1329bf077bd60bf5283e2b275ea0cc7
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-07-05 08:45:53 -0300

    Liskov Substitution Principle

--- gostack-11/iniciando-back-end/src/modules/appointments/infra/typeorm/respositories/AppointmentsRepository.ts
+++ gostack-11/iniciando-back-end/src/modules/appointments/infra/typeorm/respositories/AppointmentsRepository.ts
@@ -1,15 +1,18 @@
 import { EntityRepository, Repository } from 'typeorm';
+
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentsRepository extends Repository<Appointment>
+  implements IAppointmentsRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     // Verifica se na data já existe algo cadastrado
     const findAppointment = await this.findOne({
       where: { date },
     });
 
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
