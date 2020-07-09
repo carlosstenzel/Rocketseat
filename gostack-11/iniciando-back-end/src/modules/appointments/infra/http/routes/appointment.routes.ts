f51c0a3abb9fc7bb89d048c48bcb3efbeae0f5b6
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-07-08 21:29:08 -0300

    Usando Controllers

--- gostack-11/iniciando-back-end/src/modules/appointments/infra/http/routes/appointment.routes.ts
+++ gostack-11/iniciando-back-end/src/modules/appointments/infra/http/routes/appointment.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentServices';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentController();
 
 appointmentsRouter.use(ensureAuthenticated);
 /*
@@ -14,18 +13,6 @@ appointmentsRouter.get('/', async (request, response) => {
   return response.json(appointments);
 });
 */
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
