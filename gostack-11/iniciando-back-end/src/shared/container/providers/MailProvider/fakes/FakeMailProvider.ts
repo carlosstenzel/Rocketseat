225a9ad01b8f72439e50f8eb53ad0faa891f731f
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-07-22 21:14:47 -0300

    Aplicanto TDD na pratica

---
+++ gostack-11/iniciando-back-end/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -0,0 +1,12 @@
+import IMailProvider from '../models/IMailProvider';
+
+interface IMessage {
+  to: string;
+  body: string;
+}
+
+export default class FakeMailProvider implements IMailProvider {
+  private messages: IMessage[] = [];
+
+  public async sendMail(to: string, body: string): Promise<void> {}
+}
